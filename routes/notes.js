a65b2deecf44aa92373d105efb297a8a9ff76d04
jessicamcg <jessicamcguico@gmail.com>
2021-10-21 09:37:21 -0500

    user can append to db, does not render on screen yet

--- routes/notes.js
+++ routes/notes.js
@@ -5,20 +5,30 @@ const fs = require('fs');
 
 notes.get('/', (req, res) => res.json(noteData));
 
-notes.post('/notes', (req, res) => {
+notes.post('/', (req, res) => {
     console.log(req.body);
-  
 
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-        if (err) throw err;
-        const noteData = JSON.parse(data);
-        // console.log(noteData);
-        noteData.push(newNote);
-        fs.writeFile("./db/db.json", JSON.stringify(noteData,null,4), err => {
-          console.log("added to the file");
+    const { title, text } = req.body;
+
+    if (req.body) {
+
+        const newNote = {
+            title,
+            text,
+        };
+
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            const noteData = JSON.parse(data);
+            // console.log(noteData);
+            noteData.push(newNote);
+            fs.writeFile("./db/db.json", JSON.stringify(noteData,null,4), err => {
+            console.log("Note added to the file");
+            })
         })
-  
-    })
+    } else {
+        res.error('Error in adding note')
+    }
 
 });
 
