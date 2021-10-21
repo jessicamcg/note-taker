21b88f8f585b01e23906fbf8fe283b56c19b38eb
jessicamcg <jessicamcguico@gmail.com>
2021-10-21 17:31:14 -0500

    wildcard route returns to homepage

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use('/api', api)
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-);
-
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
